6e30ac7f48ec73322bbc3cdf4c283742c897a453
Jason Ullrich <jasonullrich@gmail.com>
2024-07-08 15:21:44 -0700

    Initial setup

---
+++ esbuild.js
@@ -0,0 +1,14 @@
+import * as esbuild from 'esbuild'
+import { tailwindPlugin } from 'esbuild-plugin-tailwindcss'
+
+const context = await esbuild.context({
+  entryPoints: ['src/client/index.js', 'src/client/index.html'],
+  bundle: true,
+  outdir: './dist',
+  loader: {'.js': 'jsx', '.html': 'copy', '.css': 'css'},
+  plugins: [
+    tailwindPlugin({})
+  ]
+})
+
+await context.watch()
