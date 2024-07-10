a20fadd5865d4922f4d0e9f6c0db670f01422470
Jason Ullrich <jasonullrich@gmail.com>
2024-07-10 13:00:58 -0700

    Tweak player count limit

--- src/server/index.js
+++ src/server/index.js
@@ -18,7 +18,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 app.use(express.static(path.join(__dirname, '../../dist')))
 
-const PLAYER_COUNT = 1
+const PLAYER_COUNT = 2
 
 const playerConnections = {}
 const playerData = {}
@@ -28,127 +28,132 @@ let gameLoop
 io.on('connection', (socket) => {
   console.log('connected')
 
-  const pc = new RTCPeerConnection({
-    iceServers: [
-      {
-        urls: [
-          'stun:stun1.l.google.com:19302',
-          'stun:stun3.l.google.com:19302',
-        ],
-      },
-    ],
-  })
+  if (Object.keys(playerNums).length < PLAYER_COUNT) {
+    const pc = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: [
+            'stun:stun1.l.google.com:19302',
+            'stun:stun3.l.google.com:19302',
+          ],
+        },
+      ],
+    })
 
-  playerConnections[socket.id] = {
-    pc,
-    ready: false,
-  }
-
-  let num
-  for (let i = 0; i < PLAYER_COUNT; i++) {
-    if (!playerNums[i]) {
-      playerNums[i] = true
-      num = i
-      break
-    }
-  }
-
-  console.log(playerNums)
-
-  playerData[socket.id] = {
-    id: socket.id,
-    num,
-    position: {},
-  }
-
-  console.log(Object.keys(playerData))
-
-  socket.emit('currentPlayers', playerData)
-
-  pc.ondatachannel = ({ channel: dataChannel }) => {
-    playerConnections[socket.id].dataChannel = dataChannel
-
-    dataChannel.onopen = () => {
-      console.log('data channel open')
+    playerConnections[socket.id] = {
+      pc,
+      ready: false,
     }
 
-    dataChannel.onmessage = ({ data }) => {
-      const message = JSON.parse(data)
-      // console.log(message)
-      if (message.position) {
-        playerData[message.id].position = message.position
-        playerData[message.id].rotation = message.rotation
+    let num
+    for (let i = 0; i < PLAYER_COUNT; i++) {
+      if (!playerNums[i]) {
+        playerNums[i] = true
+        num = i
+        break
       }
     }
-  }
 
-  socket.on('signal', async ({ description, candidate }) => {
-    if (description) {
-      console.log('recieved offer:', description)
-      await pc.setRemoteDescription(description)
-      const answer = await pc.createAnswer()
-      console.log('created answer:', answer)
-      await pc.setLocalDescription(answer)
-      socket.emit('signal', { description: answer })
-    } else if (candidate) {
-      console.log('recieved candidate:', candidate)
-      try {
-        await pc.addIceCandidate(candidate)
-        console.log('added candidate')
-      } catch (err) {
-        console.log(err)
+    console.log(playerNums)
+
+    playerData[socket.id] = {
+      id: socket.id,
+      num,
+      position: {},
+    }
+
+    console.log(Object.keys(playerData))
+
+    socket.emit('currentPlayers', playerData)
+
+    pc.ondatachannel = ({ channel: dataChannel }) => {
+      playerConnections[socket.id].dataChannel = dataChannel
+
+      dataChannel.onopen = () => {
+        console.log('data channel open')
+      }
+
+      dataChannel.onmessage = ({ data }) => {
+        const message = JSON.parse(data)
+        // console.log(message)
+        if (message.position) {
+          playerData[message.id].position = message.position
+          playerData[message.id].rotation = message.rotation
+        }
       }
     }
-  })
 
-  pc.onicecandidate = ({ candidate }) => {
-    console.log('generated candidate:', candidate)
-    if (candidate) {
-      socket.emit('signal', { candidate })
-    }
-  }
+    socket.on('signal', async ({ description, candidate }) => {
+      if (description) {
+        console.log('recieved offer:', description)
+        await pc.setRemoteDescription(description)
+        const answer = await pc.createAnswer()
+        console.log('created answer:', answer)
+        await pc.setLocalDescription(answer)
+        socket.emit('signal', { description: answer })
+      } else if (candidate) {
+        console.log('recieved candidate:', candidate)
+        try {
+          await pc.addIceCandidate(candidate)
+          console.log('added candidate')
+        } catch (err) {
+          console.log(err)
+        }
+      }
+    })
 
-  socket.on('playerName', async (name) => {
-    console.log(name)
-    playerData[socket.id].name = name
-    playerConnections[socket.id].ready = true
-    socket.emit('join', socket.id)
-    // socket.broadcast.emit('playerJoin', playerData[socket.id])
-    io.emit('playerJoin', { [socket.id]: playerData[socket.id] })
-
-    if (
-      Object.keys(playerConnections).length === PLAYER_COUNT &&
-      Object.values(playerConnections).every((player) => player.ready)
-    ) {
-      io.emit('status', {
-        state: 'countdown',
-        data: { startTime: Date.now() + 3000 },
-      })
-
-      setTimeout(() => {
-        io.emit('status', { state: 'race' })
-      }, 3030)
-
-      if (!gameLoop) {
-        gameLoop = setInterval(() => {
-          for (let player of Object.values(playerConnections)) {
-            if (player.dataChannel && player.dataChannel.readyState === 'open')
-              player.dataChannel.send(JSON.stringify({ players: playerData }))
-          }
-        }, 1000 / 120)
+    pc.onicecandidate = ({ candidate }) => {
+      console.log('generated candidate:', candidate)
+      if (candidate) {
+        socket.emit('signal', { candidate })
       }
     }
-  })
 
-  socket.on('disconnect', () => {
-    delete playerData[socket.id]
-    delete playerConnections[socket.id]
-    delete playerNums[num]
+    socket.on('playerName', async (name) => {
+      console.log(name)
+      playerData[socket.id].name = name
+      playerConnections[socket.id].ready = true
+      socket.emit('join', socket.id)
+      // socket.broadcast.emit('playerJoin', playerData[socket.id])
+      io.emit('playerJoin', { [socket.id]: playerData[socket.id] })
 
-    if (Object.keys(playerData).length === 0) {
-      clearInterval(gameLoop)
-    }
-  })
+      if (
+        Object.keys(playerConnections).length === PLAYER_COUNT &&
+        Object.values(playerConnections).every((player) => player.ready)
+      ) {
+        io.emit('status', {
+          state: 'countdown',
+          data: { startTime: Date.now() + 3000 },
+        })
+
+        setTimeout(() => {
+          io.emit('status', { state: 'race' })
+        }, 3030)
+
+        if (!gameLoop) {
+          gameLoop = setInterval(() => {
+            for (let player of Object.values(playerConnections)) {
+              if (
+                player.dataChannel &&
+                player.dataChannel.readyState === 'open'
+              )
+                player.dataChannel.send(JSON.stringify({ players: playerData }))
+            }
+          }, 1000 / 60)
+        }
+      }
+    })
+
+    socket.on('disconnect', () => {
+      delete playerData[socket.id]
+      delete playerConnections[socket.id]
+      delete playerNums[num]
+
+      if (Object.keys(playerData).length === 0) {
+        clearInterval(gameLoop)
+      }
+    })
+  }
 })
 
 server.listen(PORT, () => {
