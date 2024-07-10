ba2800c59ab658051fe8386e6554d51cb328f9b0
Jason Ullrich <jasonullrich@gmail.com>
2024-07-10 12:09:56 -0700

    Still trying

--- esbuild.js
+++ esbuild.js
@@ -2,17 +2,19 @@ import 'dotenv/config'
 import * as esbuild from 'esbuild'
 import { tailwindPlugin } from 'esbuild-plugin-tailwindcss'
 
-const context = await esbuild.context({
+const config = {
   entryPoints: ['src/client/index.js', 'src/client/index.html'],
   bundle: true,
   sourcemap: true,
   outdir: './dist',
   loader: { '.js': 'jsx', '.html': 'copy', '.css': 'css' },
   plugins: [tailwindPlugin({})],
-})
+}
 
 if (process.env.NODE_ENV === 'development') {
+  const context = await esbuild.context(config)
   await context.watch()
 } else {
+  await esbuild.build(config)
   process.exit()
 }
